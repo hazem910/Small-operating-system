ddeb8491e3b77eef8a74fc133b40a1b6c5e247a2
hazem910 <hazemibrahim911@gmail.com>
2020-06-07 14:12:37 +0200

    TMU TESTED

---
+++ TIME-MANGMENT-UINT/TMU_Testing/Error_Handling.h
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : Error_Handling.h
+ Author      : Muhammed Gamal
+ Description : 
+ ============================================================================
+ */
+
+#ifndef ERROR_HANDLING_H_
+#define ERROR_HANDLING_H_
+
+#define  TMU_INIT_NULL_POINTER     -10
+#define  TMU_MULTIPLE_INIT         -9
+#define  TMU_MODULE_NOT_INIT       -8
+#define  TMU_BUFFER_IS_EMPTY       -7
+#define  TMU_BUFFER_IS_FULL        -6
+#define  TMU_NOT_PERODICORONESHOT  -5
+#define  TMU_FUN_NULL_POINTER      -4
+#define  TMU_MULTIPLE_STOP         -3
+
+
+#endif /* ERROR_HANDLING_H_ */
--- TMU_postconfg.c
+++
@@ -1,9 +0,0 @@
-/*
- * TMU_confg.c
- *
- * Created: 2/24/2020 6:20:59 PM
- *  Author: hazem
- */ 
-
-
-
